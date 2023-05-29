9dade5e89e613bff0d5014b65ddb95613fff582c
JustMrMendez <mrmendez@amvestor.com>
2023-05-29 16:18:50 -0400

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 2. Append the path for the Skeleton NPM package and files:
+		require('path').join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+
+	plugins: [forms, typography, ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()]
+};
+
+module.exports = config;
